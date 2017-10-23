610afff2d69ff556a1027398bd9465bbee0d9461
olya <olya02071998@mail.ru>
2017-10-23 18:10:47 +0300

    Pashe

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -11,11 +11,12 @@
 
 void main()
 {
-  TMatrix<int> a(5), b(5), c(5);
-  int i, j;
+ // TMatrix<int> a(5), b(5), c(5);
+  TVector <double> vec1, vec2;
+  int  n;
 
   setlocale(LC_ALL, "Russian");
-  cout << "Тестирование программ поддержки представления треугольных матриц"
+  /*cout << "Тестирование программ поддержки представления треугольных матриц"
     << endl;
   for (i = 0; i < 5; i++)
     for (j = i; j < 5; j++ )
@@ -26,7 +27,22 @@ void main()
   c = a + b;
   cout << "Matrix a = " << endl << a << endl;
   cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
-  cout << "hello my friend"
+  cout << "Matrix c = a + b" << endl << c << endl;*/
+  cout<<"введите размер\n";
+  cin>> n;
+  cout<<"заполнение вектора 1"<<endl;
+  vec1.zap_1(n);
+  cout<<"заполнение вектора 2"<<endl;
+  vec2.zap_1(n);
+  cout<<"вектор 1"<<endl;
+  for (int i=0; i<n; i++)
+	  cout<<vec1[i]<<endl;
+  cout<<"вектор 2"<<endl;
+  for (int i=0; i<n; i++)
+	  cout<<vec2[i]<<endl;
+  /*cout<<"сумма векторов"<<endl;
+  (vec1+vec2).pechv();*/
+
+
 }
 //---------------------------------------------------------------------------
